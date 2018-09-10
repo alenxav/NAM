4b75db8016444e441824912d099d6f763b7112f9
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2018-09-10 14:28:32 -0500

    Update Functions.cpp

--- src/Functions.cpp
+++ src/Functions.cpp
@@ -771,7 +771,7 @@ NumericVector SPC(NumericVector y, NumericVector blk, NumericVector row, Numeric
   Cov = Phe/Obs; return Cov;}
 
 // [[Rcpp::export]]
-NumericMatrix SPM(NumericVector blk, NumericVector row, NumericVector col, int rN=2, int cN=2){
+NumericMatrix SPM(NumericVector blk, NumericVector row, NumericVector col, int rN=3, int cN=1){
   int n = blk.size(); NumericMatrix X(n,n); for(int i=0; i<n; i++){; for(int j=0; j<n; j++){
       if( (blk[i]==blk[j]) & (i>j) & (abs(row[i]-row[j])<=rN) & (abs(col[i]-col[j])<=cN) ){
         X(i,j) = 1; X(j,i) = 1; }else{ X(i,j) = 0; X(j,i) = 0; }}; X(i,i) = 0;}; return X;}
